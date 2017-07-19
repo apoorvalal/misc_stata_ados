049edf8195f2da9dadd02d7a6e97a21fd1daa086
apoorvalal <lal.apoorva@gmail.com>
2017-07-19 09:29:07 -0700

    added pkg file writer to monitor for new subroutines

---
+++ pkg_writer.py
@@ -0,0 +1,14 @@
+import os,glob
+adoroot = 'C:/Users/alal/Desktop/Research/misc_stata_ados/'
+pkgfile = 'C:/Users/alal/Desktop/Research/misc_stata_ados/lal_utilities.pkg'
+os.chdir(adoroot)
+ados = [file for file in glob.glob('*.ado')]
+documented_ados = [line.rstrip('\n')[2:] for line in open(pkgfile) if line[0]=='f']
+new_ados = list(set(ados)-set(documented_ados))
+print(new_ados)
+if new_ados:
+    with open(pkgfile,'a') as f:
+        for ado in new_ados:
+            f.write('f ' + ado + '\n')
+    f.close()
+
